75d3b92984ea8d910b4399efa8601b944e2f8d3f
Carlos Pérez Martín <cperezmrtn@gmail.com>
2023-07-06 12:00:53 +0200

    first commit

---
+++ src/components/QRScanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <qrcode-stream @decode="handleDecode" />
+  </div>
+</template>
+
+<script lang="ts">
+  export default {}
+</script>
+
+<script setup lang="ts">
+import { QrcodeStream } from "vue-qrcode-reader";
+
+
+
+const handleDecode = (result: string | null) => {
+  if (result) {
+    console.log(result);
+    // Realiza alguna acción con el resultado del escaneo del código QR
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos opcionales para el componente */
+</style>
